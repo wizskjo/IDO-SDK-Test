6ec3a220630601f8399cdd2eb3349a81272c97a6
wizskjo <skjo@wizardlab.co.kr>
2021-06-29 13:12:21 +0900

    IDoBLELib-Custom-2.36.0.jar 추가

--- app/src/main/java/com/wizardlab/ido_sdk_test/MainActivity.kt
+++ app/src/main/java/com/wizardlab/ido_sdk_test/MainActivity.kt
@@ -3,10 +3,12 @@ package com.wizardlab.ido_sdk_test
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+import com.ido.ble.BLEManager
+
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-    /*  git commit push test */
 }
